88b1f3b905279ac9f601c6d7d75557724c5b1c32
kanekirinookami159-coder <kanekirinookami159@gmail.com>
2026-05-19 01:04:19 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,12 +1,12 @@
 <?php
 
-$conn = mysqli_connect(
-    "ballast.proxy.rlwy.net",
-    "root",
-    "YAvrNoPpnQWBCrEeoVAZYTvfseuNeuKp",
-    "railway",
-    21696
-);
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$password = getenv("MYSQLPASSWORD");
+$database = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
+
+$conn = mysqli_connect($host, $user, $password, $database, $port);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
